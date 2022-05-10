c0e690291e38f2a3f56e9599b41866fc10ba99c5
Ezequiassantos11 <ezequiasmg16@gmail.com>
2022-05-10 16:56:39 -0300

    melhorias no cardápio

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route } from "react-router-dom";
-import { BoloInfo } from "../pages/BoloInfo";
 import { Home } from "../pages/Home";
 
 
@@ -7,7 +6,6 @@ export function Routes(){
   return(
     <BrowserRouter>
       <Route path={"/"} exact component={()=><Home/>} />
-      <Route path={`/cardapio/:title`} component={()=><BoloInfo/>} />
     </BrowserRouter>
   )
 }
